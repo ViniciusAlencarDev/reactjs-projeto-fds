f5ccf3a1d1e5af8870940ff97ea7c7cb8dbe890b
EltonLisboa <elton.lisboa@outlook.com>
2020-05-09 22:13:13 -0300

    Atualização tela home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
 import './style.css';
+import Logo from '../../assets/images/logo.png';
+import CarouselImg from '../../assets/images/carousel.jpeg';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Home() {
     return(
-        <>
-            <h1>Home</h1>
+        <>            
+        <div id='container'>
+            <img src= {Logo}/>
+            <nav>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Início</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Solicitar/Agendar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Histórico</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Vagas Disponíveis</a>
+                </li>
+            </ul>
+            </nav>
+            
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={CarouselImg}
+                    alt="First slide"
+                />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={CarouselImg}
+                alt="Third slide"
+            />
+        </Carousel.Item>
+        
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={CarouselImg}
+                alt="Third slide"
+            />
+        </Carousel.Item>
+        </Carousel>
+        <section id= 'introducao'> 
+            <h1>Estacionamento</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero corrupti nihil repellendus dolor quibusdam ducimus soluta possimus odit doloribus cumque nisi iste beatae ab a, architecto neque ad ea commodi.</p>
+        </section>  
+        </div>
+        <footer>
+            Desenvolvido por: @Alef.Ribeiro, @ViniciusAlencarDev e @EltonLisboa.
+        </footer>
         </>
     );
 }
