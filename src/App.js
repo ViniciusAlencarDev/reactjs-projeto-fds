b8c9a14fc19e383be0b599e94eb0327c0cf32645
Vinícius Alencar <viniciusalencardev@gmail.com>
2020-05-09 23:48:01 -0300

    Layou da primeira tela finalizado, realizando ajustes de cores e alinhamentos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 //Importando as paginas
 import Home from './pages/Home';
@@ -16,7 +16,13 @@ function App() {
         <Route path="/solicitacao" component={Solicitacao} />
         <Route path="/historico" component={Historico} />
         <Route path="/vagas" component={Vagas} />
-        <Route path="*" component={() => <center><h2>Pagina nao encontrada</h2></center>} />
+        <Route path="*" component={() => (
+          <center>
+            <br></br>
+            <h1>Ops! Pagina não encontrada.</h1>
+            <span>Voltar para o <Link to="/">Início</Link></span>
+          </center>
+        )} />
       </Switch>
     </Router>
   );
