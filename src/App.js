fcb83aed95b60d42f67a5caf200861d72cad5848
Vinícius Alencar <viniciusalencardev@gmail.com>
2020-05-09 21:22:50 -0300

    Rotas criadas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+//Importando as paginas
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    
-   
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="*" component={() => <center><h2>Pagina nao encontrada</h2></center>} />
+      </Switch>
+    </Router>
   );
 }
 
